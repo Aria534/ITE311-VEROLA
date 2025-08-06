c55aa0591d40f541ff4ac66dce84f6710cbe9671
Aria534 <airaverola0305@gmail.com>
2025-08-06 10:25:20 +0800

    Added Bootstrap template and Home controller

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,4 +6,3 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
-$routes->get('template', 'Home::template'); // 👈 This line allows access to /template
